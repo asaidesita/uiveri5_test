9b82c081c566b45dad01a7da29fbd5ef1fcdcad2
Ryo Asai <ryo.asai@sap.com>
2021-10-05 00:05:42 +0000

    init

---
+++ pages/home.js
@@ -0,0 +1,12 @@
+module.exports = createPageObjects({
+  Home: {
+    actions: {
+      // add action functions here
+      
+    },
+    assertions: {
+      // add assertion functions here
+      
+    }
+  }
+});
---
+++ teched.spec.js
@@ -0,0 +1,19 @@
+require("./pages/home");
+
+describe("teched", function () {
+
+  it("Should validate the home screen", function () {
+    // add actions and assertions here
+
+    expect(true).toBeTruthy();
+  });
+
+  it("should see the Home page", function () {
+    // call the page object's actions and assertions:
+    // When.onTheHomePage.iDoSomething();
+    // Then.onTheHomePage.iAssertSomething();
+  });
+
+
+
+});
